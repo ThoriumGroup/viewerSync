80ec33e6b70509136529d6e9838eb12fa84a9f74
Philippe Huberdeau <philpma@free.fr>
2014-06-27 20:06:57 -0700

    Initial release on Nukepedia

---
+++ viewerSync/viewerSync.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# philhub 2011   -   contact at philpma.AT.free.DOT.fr
+
+import nuke
+
+def viewerSync() :
+    av = nuke.activeViewer()
+    avn = av.node()
+    avi = av.activeInput()
+    curN = avn.input(avi)
+    if avn['name'].value() == 'Viewer1Sync' :
+        vSync = nuke.toNode('Viewer1')
+    else :
+        vSync = nuke.toNode('Viewer1Sync')
+    vSync.setInput( 0, curN )
+    vSync.setInput( avi, curN )
+    
+def viewerSyncCBs() :
+    vSync = nuke.toNode('Viewer1')
+    vSync['knobChanged'].setValue('viewerSync.viewerSync()')
+    if not (nuke.toNode('Viewer1Sync')) :
+        nuke.nodes.Viewer( name = 'Viewer1Sync' )
+    vSync = nuke.toNode('Viewer1Sync')
+    vSync['knobChanged'].setValue('viewerSync.viewerSync()')
+    
+def viewerSyncOFF() :
+    vSync = nuke.toNode('Viewer1')
+    vSync['knobChanged'].setValue('')
+    if (nuke.toNode('Viewer1Sync')) :
+        vSync = nuke.toNode('Viewer1Sync')
+        vSync['knobChanged'].setValue('')
